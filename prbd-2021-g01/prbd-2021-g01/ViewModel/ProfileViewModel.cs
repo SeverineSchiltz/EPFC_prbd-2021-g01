6dedbd8e17eba7c6dd63be3d47526379e7427f5c
Ines Boudghene <ines.boudghene@gmail.com>
2021-05-29 22:47:20 +0200

    feat(Profile): updates in View and ViewModel

--- prbd-2021-g01/prbd-2021-g01/ViewModel/ProfileViewModel.cs
+++ prbd-2021-g01/prbd-2021-g01/ViewModel/ProfileViewModel.cs
@@ -3,52 +3,89 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Microsoft.EntityFrameworkCore;
 using prbd_2021_g01.Model;
+using prbd_2021_g01.Properties;
 using PRBD_Framework;
 
 namespace prbd_2021_g01.ViewModel {
     public class ProfileViewModel : ViewModelCommon {
 
-        /*private User user;
-        public User User { get => user; set => SetProperty(ref user, value); }*/
+        public User User { get => CurrentUser; }
 
-        //public string Firstname { get; set; }
-        //public string Lastname { get; set; }
-        /*public string Email { get; set; }
-        public string Password { get; set; }*/
+        private string firstname;
+        public string Firstname { get => firstname; set => SetProperty<string>(ref firstname, value); }
 
-        //private string firstname;
-        public string Firstname {
-            get { return CurrentUser.Firstname; }
-            set { // => SetProperty(ref firstname, value);
-                CurrentUser.Firstname = value;
-                RaisePropertyChanged(nameof(Firstname));
+        private string lastname;
+        public string Lastname { get => lastname; set => SetProperty<string>(ref lastname, value); }
+
+        private string email;
+
+        public string Email { get => email; set => SetProperty(ref email, value, () => Validate()); }
+
+        private string password;
+
+        public string Password { get => password; set => SetProperty<string>(ref password, value, () => Validate()); }
+        
+        private string passwordConfirm;
+
+        public string PasswordConfirm { get => passwordConfirm; set => SetProperty<string>(ref passwordConfirm, value, () => Validate()); }
+
+        public ICommand SaveCommand { get; set; }
+        public ICommand CancelCommand { get; set; }
+
+        public ProfileViewModel() : base() {
+            firstname = User.Firstname;
+            lastname = User.Lastname;
+            email = User.Email;
+            password = User.Password;
+            SaveCommand = new RelayCommand(
+                SaveAction,
+                () => { return email != null && password != null && passwordConfirm != null && !HasErrors; });
+            CancelCommand = new RelayCommand(CancelAction);
+
+        }
+        // add a comment for password confirm
+        public override bool Validate() {
+            ClearErrors();
+
+            if (string.IsNullOrEmpty(Email))
+                AddError(nameof(Email), Resources.Error_Required);
+            else if (Email.Length < 3)
+                AddError(nameof(Email), Resources.Error_LengthGreaterEqual3);
+            else {
+                if (string.IsNullOrEmpty(Password))
+                    AddError(nameof(Password), Resources.Error_Required);
+                else if (Password.Length < 3)
+                    AddError(nameof(Password), Resources.Error_LengthGreaterEqual3);
+                else {
+                    if (string.IsNullOrEmpty(PasswordConfirm))
+                        AddError(nameof(PasswordConfirm), Resources.Error_Required);
+                    else if (Password.Length < 3)
+                        AddError(nameof(PasswordConfirm), Resources.Error_LengthGreaterEqual3);
+                }
             }
+            
+            RaiseErrors();
+            return !HasErrors;
         }
 
-        //private string lastname;
-        public string Lastname {
-            get { return CurrentUser.Lastname; }
-            set { // => SetProperty(ref lastname, value);
-                CurrentUser.Lastname = value;
-                RaisePropertyChanged(nameof(Lastname));
+        private void SaveAction() {
+            if (Validate()) {
+                User.Email = Email;
+                User.Password = Password;
+                Context.SaveChanges();
             }
+            //NotifyColleagues(AppMessages.MSG_PROFILE_UPDATED, User);
         }
 
-        public string Email {
-            get { return CurrentUser.Email; }
-            set { // => SetProperty(ref email, value);
-                CurrentUser.Email = value;
-                RaisePropertyChanged(nameof(Email));
-            }
-        }
-
-        public string Password {
-            get { return CurrentUser.Password; }
-            set { // => SetProperty(ref password, value);
-                CurrentUser.Password = value;
-                RaisePropertyChanged(nameof(Password));
-            }
+        private void CancelAction() {
+            Email = User.Email;
+            Password = User.Password;
+            PasswordConfirm = User.Password;
+            //Context.Reload(CurrentUser); // not working - reload with object to load data from db
+            RaisePropertyChanged(); // notify the view that all bound properties need to be refreshed 
         }
 
         protected override void OnRefreshData() {
