fb7ec980a923b95fd8c34488be263017c8c15951
Severine Schiltz <schiltz.severine@gmail.com>
2021-05-30 12:25:20 +0200

    feat(quizzes view for teacher)

---
+++ prbd-2021-g01/prbd-2021-g01/ViewModel/TeacherCourseQuizzesViewModel.cs
@@ -0,0 +1,37 @@
+﻿using prbd_2021_g01.Model;
+using PRBD_Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prbd_2021_g01.ViewModel
+{
+    public class TeacherCourseQuizzesViewModel : ViewModelCommon
+    {
+        public ICollectionView QuizzView => Quizz.GetCollectionView(nameof(Quiz.EndDateTime), ListSortDirection.Descending);
+
+
+        private ObservableCollectionFast<Quiz> quizz = new ObservableCollectionFast<Quiz>();
+        public ObservableCollectionFast<Quiz> Quizz
+        {
+            get => quizz;
+            //set => SetProperty(ref quizz, value);
+        }
+
+        private Course course;
+        public Course Course
+        {
+            get => course;
+            set => SetProperty(ref course, value, OnRefreshData);
+        }
+
+        protected override void OnRefreshData()
+        {
+            Quizz.Reset(Quiz.GetQuizzTeacher(Course));
+            //throw new NotImplementedException();
+        }
+    }
+}
